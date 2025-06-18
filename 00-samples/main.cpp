34aeebd9469627c1c6041d3ed1e79aef32fdd9d1
BuuHQ-Dev <buuhq.dev@gmail.com>
2025-06-18 11:18:37 +0700

    cm2

---
+++ 00-samples/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        (void)!freopen("input.txt", "r", stdin);
+        (void)!freopen("output.txt", "w", stdout);
+    #endif
+
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b) << endl;
+
+    return 0;
+}
